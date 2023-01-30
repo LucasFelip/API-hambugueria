14b7c555b07de85059b9878481c57e5964f6e705
lfrferreira <lfrferreira@tjma.jus.br>
2023-01-30 09:26:16 -0300

    Created UsuarioService

--- src/main/java/com/am/hambuqueria/domain/service/UsuarioService.java
+++ src/main/java/com/am/hambuqueria/domain/service/UsuarioService.java
@@ -1,20 +1,38 @@
 package com.am.hambuqueria.domain.service;
 
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.am.hambuqueria.domain.exception.NegocioException;
 import com.am.hambuqueria.domain.model.Usuario;
 import com.am.hambuqueria.domain.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-import lombok.Builder;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
 
-@Builder
 @Service
 public class UsuarioService {
+    @Autowired
+    private UsuarioRepository repository;
 
+    public List<Usuario> listAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Usuario> findUserById(Integer id) {
+        return repository.findById(id);
+    }
+
+    public List<Usuario> findUserByName(String name) {
+        return repository.findByNomeContaining(name);
+    }
+
+    @Transactional
+    public Usuario saveUser(Usuario usuario) {
+        return repository.save(usuario);
+    }
+
+    @Transactional
+    public void deleteUserById(Integer id) {
+        repository.deleteById(id);
+    }
 }
