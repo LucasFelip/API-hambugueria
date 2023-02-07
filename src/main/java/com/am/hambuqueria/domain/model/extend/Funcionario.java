a989b8fb1954d8a31afeaafa5394a24fea11eaa6
Lucas Ferreira <lfrferreira@tjma.jus.br>
2023-02-07 11:39:15 -0300

    Inserted cpf

--- src/main/java/com/am/hambuqueria/domain/model/extend/Funcionario.java
+++ src/main/java/com/am/hambuqueria/domain/model/extend/Funcionario.java
@@ -4,6 +4,7 @@ import com.am.hambuqueria.domain.model.Pessoa;
 import com.am.hambuqueria.domain.model.enumeracao.FuncaoFuncionario;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Entity
@@ -11,6 +12,10 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Funcionario extends Pessoa {
+    @Size(min = 11, max = 11)
+    @NotBlank
+    private String cpf;
+
     @NotBlank
     private FuncaoFuncionario funcaoFuncionario;
 }
