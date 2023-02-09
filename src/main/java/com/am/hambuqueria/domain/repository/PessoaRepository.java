9556479c02a5223496f3d1673d13dee5d7b4c954
Lucas Ferreira <lfrferreira@tjma.jus.br>
2023-02-09 08:25:58 -0300

    Implemented tes with JUnit and Mockito to PessoaRepository

--- src/main/java/com/am/hambuqueria/domain/repository/PessoaRepository.java
+++ src/main/java/com/am/hambuqueria/domain/repository/PessoaRepository.java
@@ -5,14 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 
-    List<Pessoa> findByNomeContainingIgnoreCase(@NonNull String nome);
+    Pessoa findByNomeContainingIgnoreCase(@NonNull String nome);
 
     Pessoa findByEmail(@NonNull String email);
 
     Pessoa findById(@NonNull int expectedId);
+
 }
--- src/test/java/com/am/hambuqueria/domain/repositoryTest/PessoaRepositoryTest.java
+++ src/test/java/com/am/hambuqueria/domain/repositoryTest/PessoaRepositoryTest.java
@@ -1,5 +1,58 @@
 package com.am.hambuqueria.domain.repositoryTest;
 
-public class PessoaRepositoryTest {
+import com.am.hambuqueria.domain.model.Pessoa;
+import com.am.hambuqueria.domain.repository.PessoaRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import static org.mockito.Mockito.*;
+
+@DataJpaTest
+public class PessoaRepositoryTest {
+    PessoaRepository repository;
+    Pessoa info;
+
+    @Before
+    public void init() {
+        repository = mock(PessoaRepository.class);
+        info = Pessoa.builder()
+                .id(1)
+                .nome("Lucas Felipe")
+                .senha("123")
+                .email("luscafreis@gmail.com")
+                .endereco("rua 16 algum lugar")
+                .telefone("98988525278")
+                .build();
+    }
+
+    @Test
+    public void deveInserirNovaInfo() {
+        repository.save(info);
+        verify(repository, times(1)).save(info);
+    }
+
+    @Test
+    public void deveBuscarInfoPorNome() {
+        var finder = "Lucas";
+        when(repository.findByNomeContainingIgnoreCase(finder)).thenReturn(info);
+    }
+
+    @Test
+    public void deveBuscarInfoPorEmail() {
+        var finder = "luscafreis@gmail.com";
+        when(repository.findByEmail(finder)).thenReturn(info);
+    }
+
+    @Test
+    public void deveBuscarInfoPorId() {
+        var finder = 1;
+        when(repository.findById(finder)).thenReturn(info);
+    }
+
+    @Test
+    public void deveDeletarInfo() {
+        repository.delete(info);
+        verify(repository, times(1)).delete(info);
+    }
 }
