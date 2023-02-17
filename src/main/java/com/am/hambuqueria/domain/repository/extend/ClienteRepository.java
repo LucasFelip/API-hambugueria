7d98f06eaf0add1e9fcc18509b3a15b8cdddf2c0
Lucas Ferreira <lfrferreira@tjma.jus.br>
2023-02-17 11:52:05 -0300

    Rolback

--- src/main/java/com/am/hambuqueria/domain/repository/extend/ClienteRepository.java
+++ src/main/java/com/am/hambuqueria/domain/repository/extend/ClienteRepository.java
@@ -1,7 +1,6 @@
 package com.am.hambuqueria.domain.repository.extend;
 
 import com.am.hambuqueria.domain.model.extend.Cliente;
-import com.am.hambuqueria.domain.repository.PessoaRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public class ClienteRepository extends PessoaRepository<Cliente> {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     List<Cliente> findByNomeContainingIgnoreCase(@NonNull String nome);
 
     List<Cliente> findByCpfContainingIgnoreCase(@NonNull String cpf);
