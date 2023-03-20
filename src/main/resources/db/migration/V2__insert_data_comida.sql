1511e4769424bc080b4132bdce071906ed702f46
Lucas Ferreira <lfrferreira@tjma.jus.br>
2023-03-20 12:22:19 -0300

    Renomeado script sql

---
+++ src/main/resources/db/migration/V2__insert_data_comida.sql
@@ -0,0 +1,4 @@
+INSERT INTO comida (nome, disponibilidade, descricao, preco, ingredientes, tipo_comida)
+VALUES ('Pizza Margherita', 1, 'Pizza com mussarela, tomate e manjericão', 30, 'Farinha de trigo, água, fermento, mussarela, tomate, manjericão', 1),
+       ('Hambúrguer com batata frita', 0, 'Pão, carne bovina, alface, tomate, queijo, batata frita', 20, 'Pão, carne bovina, alface, tomate, queijo, batata, óleo', 2),
+       ('Sushi', 1, 'Salmão, arroz, nori, pepino, cream cheese', 50, 'Salmão, arroz, nori, pepino, cream cheese', 3);
